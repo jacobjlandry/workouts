9c5d5cb3560063f1423173df8d7ad82ae9bab29c
Jacob Landry <jacoblandry@Jacobs-Air.hsd1.ca.comcast.net>
2016-08-22 23:59:40 -0700

    adding some special css for mobile devices to help see more clearly

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -3,9 +3,9 @@
     <head>
         <title>Workouts</title>
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
         <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
         <link href="{{ asset('css/font-awesome.min.css') }}" rel="stylesheet" type="text/css">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css">
 
         <script src="{{ asset('js/jquery.js') }}" type="text/javascript"></script>
 
@@ -20,7 +20,7 @@
                 width: 100%;
                 display: table;
                 font-weight: 100;
-                font-family: 'Lato', sans-serif;
+                font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
             }
 
             .container {
@@ -35,41 +35,126 @@
                 margin: auto;
             }
 
-            .title {
-                font-size: 96px;
-            }
-            .subtitle {
-                font-size: 30px;
-                text-align: center;
-            }
-
-            .navbar {
-                position: fixed;
-                top: 0px;
-                left: 0px;
-                width: 100%;
-                height: 50px;
-                display: flex;
-                justify-content: flex-end;
-                background: #ffffff;
-            }
-            .navicon, .navicon a {
-                padding: 5px;
-                color: #000000;
-                font-size: 22px;
-                font-weight: bold;
-            }
             .results {
                 display: flex;
-
+            }
+            .extra-credit {
+                color: #DAA520;
+            }
+            @media (min-width: 480px) and (orientation: portrait) {
+                body {
+                    padding-left: 15px;
+                }
+                .title {
+                    margin-top: 45px;
+                    font-size: 150px;
+                }
+                .subtitle {
+                    font-size: 75px;
+                }
+                .navbar {
+                    position: fixed;
+                    top: 0px;
+                    left: 0px;
+                    width: 100%;
+                    display: flex;
+                    justify-content: flex-end;
+                    height: 80px;
+                    background: #cccccc;
+                }
+                .navicon, .navicon a {
+                    padding-right: 10px;
+                    padding-left: 10px;
+                    font-size: 50px;
+                    color: #ffffff;
+                    font-weight: bold;
+                    display: flex;
+                }
+                #create-text {
+                    display: none;
+                }
+                #logout-text {
+                    display: none;
+                }
                 .goal-img {
-                    max-width: 150px;
+                    max-width: 800px;
+                    max-height: 800px;
+                    overflow: hidden;
+                }
+            }
+            @media (min-width: 700px) and (orientation: landscape) {
+                .title {
+                    font-size: 100px;
+                }
+                .subtitle {
+                    font-size: 50px;
+                }
+                .navbar {
+                    position: fixed;
+                    top: 0px;
+                    left: 0px;
+                    width: 100%;
+                    display: flex;
+                    justify-content: flex-end;
+                    height: 30px;
+                    background: #cccccc;
+                }
+                .navicon, .navicon a {
+                    padding-right: 10px;
+                    padding-left: 10px;
+                    font-size: 30px;
+                    color: #ffffff;
+                    font-weight: bold;
+                }
+                #create-icon {
+                    display: none;
+                }
+                #logout-icon {
+                    display: none;
+                }
+                .goal-img {
+                    max-width: 250px;
                     max-height: 400px;
                     overflow: hidden;
                 }
             }
-            .extra-credit {
-                color: #DAA520;
+            @media (min-width: 1000px) {
+                .title {
+                    font-size: 130px;
+                }
+                .subtitle {
+                    font-size: 30px;
+                    text-align: center;
+                }
+
+                .navbar {
+                    position: fixed;
+                    top: 0px;
+                    left: 0px;
+                    width: 100%;
+                    height: 50px;
+                    display: flex;
+                    justify-content: flex-end;
+                    background: #ffffff;
+                }
+                .navicon, .navicon a {
+                    padding: 5px;
+                    color: #AAAAAA;
+                    font-size: 20px;
+                    font-weight: normal;
+                    display: flex;
+                }
+                #create-icon {
+                    display: none;
+                }
+                #logout-icon {
+                    display: none;
+                }
+                .goal-img {
+                    max-width: 250px;
+                    max-height: 400px;
+                    overflow: hidden;
+                }
             }
         </style>
     </head>
@@ -82,11 +167,13 @@
             @else
                 @if(Auth::user()->is('admin') || Auth::user()->is('editor'))
                     <div class="navicon">
-                        <a href="/workout/create">Create Workout</a>
+                        <div id="create-text"><a href="/workout/create">Create Workout</a></div>
+                        <div id="create-icon" style="padding: 15px;"><a href="/workout/create"><i class="fa fa-plus"></i></a></div>
                     </div>
                 @endif
                 <div class="navicon">
-                    <a href="/logout">Logout</a>
+                    <div id="logout-text"><a href="/logout">Logout</a></div>
+                    <div id="logout-icon" style="padding: 15px;"><a href="/logout"><i class="fa fa-sign-out"></i></a></div>
                 </div>
             @endif
         </div>
